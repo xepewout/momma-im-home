8473d00952a098aaa7b214d6bff3ff56e06ac1df
xepewout <111144938+xepewout@users.noreply.github.com>
2026-01-18 22:54:42 -0800

    Wall Commit
    -added wall sprite

--- wall.gd
+++ wall.gd
@@ -3,7 +3,8 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$ColorRect.color = Color(randf(), randf(), randf())
+	pass
+	#$ColorRect.color = Color(randf(), randf(), randf())
 	
 func _process(_delta: float) -> void:
 	position.y -= Global.game_speed
